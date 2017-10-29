f20ae6a87cbdd4cc798f9e8e36652fd61b34e023
dcannarozzi <dcannarozzi@gmail.com>
2017-10-28 22:23:39 -0500

    - added support for user

--- src/main/java/org/launchcode/prospector6/controllers/UserController.java
+++ src/main/java/org/launchcode/prospector6/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.launchcode.prospector6.models.Referrer;
 import org.launchcode.prospector6.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,12 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("user")
 public class UserController {
 
+    public static User currentUser = new User();
+    User[] users;
 
-    @RequestMapping(value = "")
-    public String index(Model model) {
-        // uncomment when replaced hardcoded values with UserDao
-        //User[] users = userDao.findAll();
-        User[] users = new User[2];
+    public UserController() {
+        users = new User[2];
         users[0] = new User();
         users[0].setId(1);
         users[0].setUserFirst("John");
@@ -27,10 +27,22 @@ public class UserController {
         users[1].setId(2);
         users[1].setUserFirst("Jane");
         users[1].setUserLast("Doe");
+    }
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        // uncomment when replaced hardcoded values with UserDao
+        //User[] users = userDao.findAll();
 
         model.addAttribute("users", users);
         model.addAttribute("title", "Users");
         return "user/index";
     }
 
+    @RequestMapping(value="view/{id}")
+    public String view(Model model, @PathVariable int id) {
+        // use the statically defined users for now until a DAO is created
+        currentUser = users[id-1];
+        return "referrer/index";
+    }
 }
